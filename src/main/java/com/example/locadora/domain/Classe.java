59a8b07b5045356ec193d2526a8b88d02ee0e9d4
bruno <brunocalegari7@gmail.com>
2024-10-01 20:05:08 -0300

    feat: add classes de dominio

---
+++ src/main/java/com/example/locadora/domain/Classe.java
@@ -0,0 +1,29 @@
+package com.example.locadora.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Classe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Double valor;
+
+    @Temporal(TemporalType.DATE)
+    private Date prazoDevolucao;
+
+}
