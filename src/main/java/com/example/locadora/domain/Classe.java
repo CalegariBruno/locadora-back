7ac772c61f7cc97a8c71189c948a9ae3f6f61fd1
bruno <brunocalegari7@gmail.com>
2024-10-16 17:41:44 -0300

    feat: add classes do controlador

--- src/main/java/com/example/locadora/domain/Classe.java
+++ src/main/java/com/example/locadora/domain/Classe.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -24,7 +25,6 @@ public class Classe {
     private Double valor;
 
     @Column(name = "prazo_devolucao")
-    @Temporal(TemporalType.DATE)
-    private Date prazoDevolucao;
+    private LocalDate prazoDevolucao;
 
 }
