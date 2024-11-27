f4c14ed71fdb144beceaac2516dd5b86caa4c4a7
CalegariBruno <brunocalegari7@gmail.com>
2024-11-27 16:46:03 -0300

    feat add funcionalidades de cliente no service e controller

--- src/main/java/com/example/locadora/controllers/ClienteController.java
+++ src/main/java/com/example/locadora/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.locadora.controllers;
 
 
 import com.example.locadora.domain.Ator;
+import com.example.locadora.domain.Classe;
 import com.example.locadora.domain.Cliente;
 import com.example.locadora.domain.Dependente;
 import com.example.locadora.domain.Socio;
@@ -29,6 +30,20 @@ public class ClienteController {
         return ResponseEntity.ok(clientes);
     }
 
+    @GetMapping("/listarSocios")
+    @Operation(description = "Lista todos os socios.")
+    public ResponseEntity<List<Socio>> listarSocios() {
+        List<Socio> socios = clienteService.listarTodosSocios();
+        return ResponseEntity.ok(socios);
+    }
+
+    @GetMapping("/listarDependentes")
+    @Operation(description = "Lista todos os dependentes.")
+    public ResponseEntity<List<Dependente>> listarDependentes() {
+        List<Dependente> dependentes = clienteService.listarTodosDependentes();
+        return ResponseEntity.ok(dependentes);
+    }
+
     @PostMapping("/socio/criar")
     @Operation(description = "Dado o nome, cadastra um socio.", responses = {
             @ApiResponse(responseCode = "200", description = "Caso o socio seja inserido com sucesso."),
@@ -51,6 +66,47 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(novoDependente);
     }
 
+    @PutMapping("/socio/editar/{id}")
+    @Operation(description = "Dado o id, a classe é editada.", responses = {
+            @ApiResponse(responseCode = "200", description = "Caso a classe seja editada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "O servidor não pode processar a requisição devido a alguma coisa que foi entendida como um erro do cliente."),
+            @ApiResponse(responseCode = "500", description = "Caso não tenha sido possível realizar a operação.")
+    })
+    public ResponseEntity<Socio> editarSocio(@PathVariable Long id, @RequestBody Socio socioAtualizada) {
+        Socio socioEditada = clienteService.editarSocio(id, socioAtualizada);
+        return ResponseEntity.ok(socioEditada);
+    }
 
+    @PutMapping("/dependente/editar/{id}")
+    @Operation(description = "Dado o id, a classe é editada.", responses = {
+            @ApiResponse(responseCode = "200", description = "Caso a classe seja editada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "O servidor não pode processar a requisição devido a alguma coisa que foi entendida como um erro do cliente."),
+            @ApiResponse(responseCode = "500", description = "Caso não tenha sido possível realizar a operação.")
+    })
+    public ResponseEntity<Dependente> editarDependente(@PathVariable Long id, @RequestBody Dependente dependenteAtualizada) {
+        Dependente dependenteEditada = clienteService.editarDependente(id, dependenteAtualizada);
+        return ResponseEntity.ok(dependenteEditada);
+    }
+
+    @GetMapping("/buscar/{id}")
+    @Operation(description = "Busca ator por ID.")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        Cliente cliente = clienteService.buscarPorId(id);
+        return ResponseEntity.ok(cliente);
+    }    
+
+    @DeleteMapping("/deletarSocio/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(description = "Deleta o ator dado o ID.")
+    public void deletarSocio(@PathVariable Long id) throws Exception{
+        clienteService.deletarSocio(id);
+    }
+
+    @DeleteMapping("/deletarDependente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(description = "Deleta o ator dado o ID.")
+    public void deletarDependente(@PathVariable Long id) throws Exception{
+        clienteService.deletarDependente(id);
+    }
 
 }
--- src/main/java/com/example/locadora/services/ClienteService.java
+++ src/main/java/com/example/locadora/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.example.locadora.services;
 
+import com.example.locadora.domain.Ator;
 import com.example.locadora.domain.Cliente;
 import com.example.locadora.domain.Dependente;
 import com.example.locadora.domain.Socio;
@@ -7,9 +8,12 @@ import com.example.locadora.repositories.ClienteRepository;
 import com.example.locadora.repositories.DependenteRepository;
 import com.example.locadora.repositories.SocioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -22,11 +26,7 @@ public class ClienteService {
     private SocioRepository socioRepository;
 
     @Autowired
-    private DependenteRepository dependenteRepository;
-
-    public List<Cliente> listarTodosClientes(){
-        return clienteRepository.findAll();
-    }
+    private DependenteRepository dependenteRepository;    
 
     public Socio criarSocio(Socio socio){
 
@@ -44,7 +44,76 @@ public class ClienteService {
         return dependenteRepository.save(dependente);
     }
 
-    public int gerarNumeroInscricao() {
+    public Socio editarSocio(Long id, Socio socioAtualizado){
+
+        Optional<Socio> socioExistente = socioRepository.findById(id);
+
+        if(socioExistente.isPresent()){
+            Socio socio = socioExistente.get();
+            socio.setNumeroInscricao(socioExistente.get().getNumeroInscricao());
+            socio.setCpf(socioAtualizado.getCpf());
+            socio.setNome(socioAtualizado.getNome());
+            socio.setDataNascimento(socioAtualizado.getDataNascimento());
+            socio.setAtivo(socioAtualizado.isAtivo());
+            socio.setSexo(socioAtualizado.getSexo());
+            socio.setLocacoes(socioExistente.get().getLocacoes());
+            socio.setTelefone(socioAtualizado.getTelefone());
+            socio.setDependentes(socioExistente.get().getDependentes());
+            return socioRepository.save(socio);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sócio não encontrado!");
+        }
+
+    }
+
+    public Dependente editarDependente(Long id, Dependente dependenteAtualizado){
+
+        Optional<Dependente> dependenteExistente = dependenteRepository.findById(id);
+
+        if(dependenteExistente.isPresent()){
+
+            Dependente dependente = dependenteExistente.get();
+            dependente.setNumeroInscricao(dependenteExistente.get().getNumeroInscricao());
+            dependente.setNome(dependenteAtualizado.getNome());
+            dependente.setDataNascimento(dependenteAtualizado.getDataNascimento());
+            dependente.setAtivo(dependenteAtualizado.isAtivo());
+            dependente.setSexo(dependenteAtualizado.getSexo());
+            dependente.setLocacoes(dependenteExistente.get().getLocacoes());
+            dependente.setSocio(dependenteAtualizado.getSocio());
+
+            return dependenteRepository.save(dependente);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Dependente não encontrado!");
+        }
+
+    }
+
+    public void deletarSocio(Long id) throws Exception{
+
+    }
+
+    public void deletarDependente(Long id) throws Exception{
+
+    }
+
+    public List<Cliente> listarTodosClientes(){
+        return clienteRepository.findAll();
+    }
+
+    public List<Socio> listarTodosSocios(){
+        return socioRepository.findAll();
+    }
+
+    public List<Dependente> listarTodosDependentes(){
+        return dependenteRepository.findAll();
+    }
+
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
+    }
+
+    private int gerarNumeroInscricao() {
 
         int numero;
         Random random = new Random();
