5c9ea7a8c5677828ae523a21269a00385439777a
bruno <brunocalegari7@gmail.com>
2024-11-28 21:13:23 -0300

    feat: add rota para listar socios disponiveis

--- src/main/java/com/example/locadora/repositories/SocioRepository.java
+++ src/main/java/com/example/locadora/repositories/SocioRepository.java
@@ -2,7 +2,16 @@ package com.example.locadora.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.locadora.domain.Socio;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SocioRepository extends JpaRepository<Socio, Long>{
     boolean existsByNumeroInscricao(int numeroInscricao);
+
+    // listar socios que possuem menos de 3 dependentes ativos
+    @Query("SELECT s FROM Socio s WHERE (SELECT COUNT(d) FROM Dependente d WHERE d.socio = s AND d.ativo = TRUE) < 3")
+    List<Socio> listarSociosComMenosDe3DependentesAtivos();
+
+
 }
