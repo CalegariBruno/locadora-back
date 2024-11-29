00f5b79c2f0c6ae26d77e25def8f1de456c1161d
bruno <brunocalegari7@gmail.com>
2024-11-28 21:13:01 -0300

    feat: add rota para listar clientes ativos

--- src/main/java/com/example/locadora/repositories/ClienteRepository.java
+++ src/main/java/com/example/locadora/repositories/ClienteRepository.java
@@ -3,6 +3,12 @@ package com.example.locadora.repositories;
 import com.example.locadora.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     boolean existsByNumeroInscricao(int numeroInscricao);
+
+    //listar somente os clientes ativos
+    List<Cliente> findByAtivoTrue();
+
 }
